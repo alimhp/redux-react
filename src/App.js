457bdee6a6f20892101927cbfb77f82a4d045499
alimhp <alimohamadipourr@gmail.com>
2022-08-13 13:13:26 -0700

    Using global states in component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import CakeContainer from "./Component/CakeContainer";
 import { Provider } from "react-redux";
 import store from "./Redux/Store";
+import HooksCakeContainer from "./Component/HooksCakeContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <CakeContainer />
+        <HooksCakeContainer />
       </div>
     </Provider>
   );
---
+++ src/Component/HooksCakeContainer.js
@@ -0,0 +1,14 @@
+import { useSelector, useDispatch } from "react-redux/es/exports";
+import { buyCake } from "../Redux/Cake/CakeAction";
+const HooksCakeContainer = (props) => {
+  const numOfCakes = useSelector((state) => state.numOfCakes);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <div>number of cake - {numOfCakes} </div>
+      <button onClick={() => dispatch(buyCake())}>buy cake</button>
+    </div>
+  );
+};
+
+export default HooksCakeContainer;
